39dec57e20ad46c3843015c0bb881bcb0a6bfed1
Sneh_Patel <79684679+snehpatel1017@users.noreply.github.com>
2023-06-13 19:57:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,62 +3,35 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import { data } from "./Data"
 import CardList from './CardList';
 import { Link } from 'react-router-dom'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   const [finallist, setfinallist] = useState([]);
+  const [searchstring, setSearchString] = useState("");
+  const [filter, setFilter] = useState("");
   var cand = [];
   var sr = [];
 
-  var category = "";
-  var search = "";
-  var EmptyMessage = "";
+
   function searchfunction(term) {
-    search = term
 
-    var list = data;
 
-    const newlist = list.filter((card) => {
-      const name = card.name.toLowerCase();
-      if (name.startsWith(term) || term === "") return true
-      else return false
-    });
-    // console.log(newlist)
-    sr = newlist
-    console.log('sr')
-    console.log(sr)
-    filterHandler(category)
 
-    // console.log(searchResult)
+    setSearchString(term)
+
+
   }
   function filterHandler(term) {
 
 
-    category = term
 
-    var list = data;
-    if (sr.length > 0) list = sr;
-    cand = list.filter((card) => {
+    setFilter(term)
 
-      if (card.card_type === term || term === "all" || term === "") return true
-      else return false
-    });
-    console.log(category)
-    console.log("cand")
-    console.log(cand)
-    if (cand.length === 0) {
-      EmptyMessage = "No data Found"
-    }
-    // console.log(EmptyMessage)
-    setfinallist(cand)
-    // finallist = newlist.concat(searchResult)
   }
 
 
-  // console.log(filterResult)
-  // finallist = searchResult.concat(filterResult)
-  // console.log(finallist)
+
 
 
   return (
@@ -76,10 +49,10 @@ function App() {
           </Link>
         </div>
         <Route path="/your" exact
-          render={(props) => <CardList {...props} cardlist={finallist.length === 0 ? data : finallist} owner_id={1} searchHandler={searchfunction} filterHandler={filterHandler} empty={EmptyMessage} />}
+          render={(props) => <CardList {...props} cardlist={data} search={searchstring} filter={filter} owner_id={1} searchHandler={searchfunction} filterHandler={filterHandler} />}
         ></Route>
         <Route path="/all" exact
-          render={(props) => <CardList {...props} cardlist={finallist.length === 0 ? data : finallist} searchHandler={searchfunction} filterHandler={filterHandler} empty={EmptyMessage} />}
+          render={(props) => <CardList {...props} cardlist={data} search={searchstring} filter={filter} searchHandler={searchfunction} filterHandler={filterHandler} />}
         ></Route>
         <Route path="/blocked" exact ></Route>
       </Router>
